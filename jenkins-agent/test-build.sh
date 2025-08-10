74c720e3057771e11e79109a2e0a3a59fb26265b
lattebreak <darkerwhite08@gmail.com>
2025-08-10 23:41:47 +0800

    overwrite agent v1.1 with docker-cli-buildx installed.

--- jenkins-agent/test-build.sh
+++ jenkins-agent/test-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # BUILD JENKINS AGENT IMAGE TO BE USED AS AGENT TEMPLATE
-docker buildx build -t jenkins-agent:test ./jenkins-agent
+docker buildx build -t jenkins-agent:v1.1 ./jenkins-agent
