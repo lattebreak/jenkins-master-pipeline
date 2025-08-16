27d1c3e8889082d29119660452b111c9fe1fcae0
lattebreak <darkerwhite08@gmail.com>
2025-08-16 12:46:34 +0800

    learning git diff as pipeline condition

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import subprocess
-import os
 
-# # Build image
-# try:
-#     build_image = subprocess.run(['./jenkins-agent/test-build.sh'], capture_output=True, text=True)
-#     build_image_stdout = build_image.stdout
-#     build_image_stderr = build_image.stderr
-#     build_image_returncode = build_image.returncode
+# Build image
+try:
+    build_image = subprocess.run(['./jenkins-agent/test-build.sh'], capture_output=True, text=True)
+    build_image_stdout = build_image.stdout
+    build_image_stderr = build_image.stderr
+    build_image_returncode = build_image.returncode
 
-#     if build_image_returncode != 0:
-#         print(f"Error during build:\n{build_image_stderr}")
-#         exit(1)
-#     else:
-#         print(f"Successful build!\n{build_image_stdout}")
+    if build_image_returncode != 0:
+        print(f"Error during build:\n{build_image_stderr}")
+        exit(1)
+    else:
+        print(f"Successful build!\n{build_image_stdout}")
 
-# except subprocess.CalledProcessError as e:
-#     print(F"ERROR:\n{e}")
+except subprocess.CalledProcessError as e:
+    print(F"ERROR:\n{e}")
